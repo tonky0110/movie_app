2e6102036aa98997a97a4b41f60c906577c9a13b
tonky0110 <tonky0110@gmail.com>
2018-04-27 00:07:56 +0900

    reactJs 만들기_6-2까지 실습

--- src/App.js
+++ src/App.js
@@ -2,34 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: 'Matrix', 
-    poster: "https://ia.media-imdb.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
-  },
-  {
-    title: 'Full Metal Jacket', 
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT27oi5sjwOuEDZf3FnwliIcv-OFiNwF8FjPtPI5XxpWIpafDnW"
-  },
-  {
-    title: 'Oldboy', 
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: 'Star Wars', 
-    poster: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/250px-Star_Wars_Logo.svg.png"
-  }
-];
-
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount();
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate();
+
+  state = {};
+  
+  componentDidMount() {
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err))
+      ;
+  }
+  
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+          return <Movie key={index} title={movie.title} poster={movie.poster} />
+        });
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
-          return <Movie key={index} title={movie.title} poster={movie.poster} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 //yarn add prop-types
 import './Movie.css';
 
-class Movie extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.String
-    };
-
-    render(){
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
-}
-
-class MoviePoster extends Component {
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-  render() {
+function Movie({title, poster}) {
     return (
-      <img src={this.props.poster} alt={this.props.title}/>
+        <div>
+            <MoviePoster title = {title} poster = {poster} />
+            <h1>{title}</h1>
+        </div>
     );
-  }
 }
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+function MoviePoster({ poster }){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    );
+};
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+};
 
 export default Movie;
+
