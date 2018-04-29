1bcaffcb695ba5e48e075f55cb9cad1b4924f2fb
tonky0110 <tonky0110@gmail.com>
2018-04-29 10:38:01 +0900

    6.3 promise
      // await : call api기능이 끝나기를 기다리는 것(not 성공적으로 수행)
      _getMovies = async () => {
        const movies = await this._callApi();
        this.setState({
          movies
        });
      }
    수정

--- src/App.js
+++ src/App.js
@@ -9,18 +9,31 @@ class App extends Component {
   state = {};
   
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err))
-      ;
+    this._getMovies();
   }
   
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-          return <Movie key={index} title={movie.title} poster={movie.poster} />
-        });
+    const movies = this.state.movies.map(movie => {
+      console.log(movie);
+      return <Movie key={movie.id} title={movie.title} poster={movie.large_cover_image} />
+    });
     return movies;
+  }
+
+  // await : call api기능이 끝나기를 기다리는 것(not 성공적으로 수행)
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies
+    });
+  }
+
+  //arrow fuctions에서 => return을 하지 않는다(자동으로 return)
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
   }
 
   render() {
